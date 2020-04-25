bb641a8304f9afed5d46c1fcc2adbe34bbd3e7c5
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-25 12:45:02 +0100

    Introduce randomness and fix player position

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const BALL_DIAMETER = 20;
 const BALL_RADIUS = BALL_DIAMETER / 2;
 const PLAYER_WIDTH = 20;
 const PLAYER_HEIGHT = 100;
+const INITIAL_SPEED = 240;
 
 class Vec {
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
+  }
+  get len() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  set len(value) {
+    const factor = value / this.len;
+    this.x *= factor;
+    this.y *= factor;
   }
 }
 
@@ -16,16 +25,16 @@ class Rect {
     this.size = new Vec(width, height);
   }
   get left() {
-    return this.pos.x - BALL_RADIUS;
+    return this.pos.x - this.size.x / 2;
   }
   get right() {
-    return this.pos.x + BALL_RADIUS;
+    return this.pos.x + this.size.x / 2;
   }
   get top() {
-    return this.pos.y - BALL_RADIUS;
+    return this.pos.y - this.size.y / 2;
   }
   get bottom() {
-    return this.pos.y + BALL_RADIUS;
+    return this.pos.y + this.size.y / 2;
   }
 }
 
@@ -49,21 +58,14 @@ class Pong {
     this._context = canvas.getContext('2d');
 
     this.ball = new Ball;
-    
-    this.ball.pos.x = 20;
-    this.ball.pos.y = 20;
-    this.ball.vel.x = 100;
-    this.ball.vel.y = 100;
+    this.reset();
 
-    this.players = [
-      new Player,
-      new Player
-    ];
+    this.players = [new Player, new Player];
 
     this.players[0].pos.x = 40;
     this.players[1].pos.x = this._canvas.width - 40;
     this.players.forEach(player => {
-      player.pos.y = this._canvas.height / 2 - PLAYER_HEIGHT / 2;
+      player.pos.y = this._canvas.height / 2;
     })
 
     let lastTime;
@@ -79,8 +81,15 @@ class Pong {
   }
 
   collide(player, ball) {
-    if (player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top) {
+    // Check if ball hit by player
+    if (player.left < ball.right && player.right > ball.left &&
+      player.top < ball.bottom && player.bottom > ball.top) {
+      const len = ball.vel.len;
       ball.vel.x = -ball.vel.x;
+      // Introduce some randomness to vertical velocity
+      ball.vel.y += INITIAL_SPEED * (Math.random() - 0.5);
+      // Increase ball's speed slightly
+      ball.vel.len = len * 1.05;
     }
   }
 
@@ -98,6 +107,24 @@ class Pong {
     this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
   }
 
+  reset() {
+    this.ball.pos.x = this._canvas.width / 2;
+    this.ball.pos.y = this._canvas.height / 2;
+
+    this.ball.vel.x = 0;
+    this.ball.vel.y = 0;
+  }
+
+  start() {
+    if (this.ball.vel.x === 0 && this.ball.vel.y === 0) {
+      // Introduce some randomness to the ball's direction of travel
+      this.ball.vel.x = INITIAL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+      this.ball.vel.y = INITIAL_SPEED * Math.random() * 2 - 1;
+      // Keep its speed consistent, though
+      this.ball.vel.len = INITIAL_SPEED;
+    }
+  }
+
   update(dt) {
     // Change ball's position relative to the time difference
     this.ball.pos.x += this.ball.vel.x * dt;
@@ -108,9 +135,11 @@ class Pong {
       this.ball.vel.y = -this.ball.vel.y;
     }
     
-    // Reverse velocity if ball bounces on the left or right
+    // Award point if ball reaches far left or right
     if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-      this.ball.vel.x = -this.ball.vel.x;
+      const playerId = this.ball.vel.x < 0 | 0;
+      this.players[playerId].score++;
+      this.reset();
     }
 
     // Computer player always follows the ball
@@ -124,41 +153,21 @@ class Pong {
 const canvas = document.getElementById('game');
 const pong = new Pong(canvas);
 
-const myScoreBox = document.getElementById('myScore');
-const computerScoreBox = document.getElementById('computerScore');
-
-let myScore = 0;
-let computerScore = 0;
-
-setMyScore(0);
-setComputerScore(0);
-
-function setMyScore(score) {
-	myScore = score;
-	myScoreBox.textContent = myScore;
-}
-
-function setComputerScore(score) {
-	computerScore = score;
-	computerScoreBox.textContent = computerScore;
-}
-
 function keyPress(event) {
-	if (event.keyCode === 38) {
+	if (event.keyCode === 38 && pong.players[0].top > 0) {
     pong.players[0].pos.y = pong.players[0].pos.y - BALL_DIAMETER;
-	} else if (event.keyCode === 40) {
+	} else if (event.keyCode === 40 && pong.players[0].bottom < canvas.height) {
 		pong.players[0].pos.y = pong.players[0].pos.y + BALL_DIAMETER;
-	}
+	} else if (event.keyCode === 32) {
+    pong.start();
+  }
 }
 
 function moveMouse(event) {
-	if (event.offsetY < canvas.height - (PLAYER_HEIGHT / 2)) {
+	if (event.offsetY > PLAYER_HEIGHT / 2 && event.offsetY < canvas.height - (PLAYER_HEIGHT / 2)) {
 		pong.players[0].pos.y = event.offsetY;
 	}
 }
-
-let direction = Math.random() > 0.5 ? 'right' : 'left';
-let yOffset = Math.random() * 10 - 5;
 
 document.addEventListener('keydown', keyPress);
 canvas.addEventListener('mousemove', moveMouse);
